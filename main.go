41e34e3a0b43ca9cab365fb1362c0da141b3fb30
fishballian <fishballian@live.com>
2024-12-09 02:33:26 +0800

    leetcode

--- main.go
+++ main.go
@@ -1,67 +1,34 @@
 package main
 
-func main() {
+var Prime []int
+var memo []int
+
+func init() {
+	isPrime := make([]bool, 1e8+1)
+	for i := 2; i <= 2e8; i++ {
+		if !isPrime[i] {
+			Prime = append(Prime, i)
+			if palindrome(i) {
+				memo = append(memo, i)
+			}
+		}
+		for _, p := range Prime {
+			if i*p > 2e8 {
+				break
+			}
+			isPrime[i*p] = true
+			if i%p == 0 {
+				break
+			}
+		}
+	}
 }
 
-func countSubstrings1(s string) int {
-	n := len(s)
-	a := make([]byte, 2*n+1)
-	for i := 0; i < n; i++ {
-		a[2*i] = '#'
-		a[2*i+1] = s[i]
+func palindrome(num int) bool {
+	val := num
+	tmp := 0
+	for val != 0 {
+		tmp, val = tmp*10+val%10, val/10
 	}
-	a[2*n] = '#'
-	d := make([]int, 2*n+1)
-	l, r := 0, -1
-	ans := 0
-	for i := 0; i < 2*n+1; i++ {
-		k := 1
-		if i <= r {
-			k = min(d[l+r-i], r-i+1)
-		}
-		for i-k >= 0 && i+k < 2*n+1 && a[i-k] == a[i+k] {
-			k++
-		}
-		d[i] = k
-		ans += k / 2
-		k--
-		if i+k > r {
-			l, r = i-k, i+k
-		}
-
-	}
-	return ans
-}
-
-func countSubstrings2(s string) int {
-	n := len(s)
-	t := "$#"
-	for i := 0; i < n; i++ {
-		t += string(s[i]) + "#"
-	}
-	n = len(t)
-	t += "!"
-
-	f := make([]int, n)
-	iMax, rMax, ans := 0, 0, 0
-	for i := 1; i < n; i++ {
-		// 初始化 f[i]
-		if i <= rMax {
-			f[i] = min(rMax-i+1, f[2*iMax-i])
-		} else {
-			f[i] = 1
-		}
-		// 中心拓展
-		for t[i+f[i]] == t[i-f[i]] {
-			f[i]++
-		}
-		// 动态维护 iMax 和 rMax
-		if i+f[i]-1 > rMax {
-			iMax = i
-			rMax = i + f[i] - 1
-		}
-		// 统计答案, 当前贡献为 (f[i] - 1) / 2 上取整
-		ans += f[i] / 2
-	}
-	return ans
+	return tmp == num
 }
--- note/go.md
+++ note/go.md
@@ -2,4 +2,10 @@ go方法分为value receivers和pointer receivers
 1、两者大多时候都能自动转换，比如指针能调用value方法，数值能调用pointer方法  
 2、但是如果要修改原数据，只能使用pointer方法，因为value方法是拷贝。  
 3、对于大结构，pointer方法能减少拷贝消耗，但是使用不当可能会导致引用外泄增大内存消耗  
-4、对于立即数或者其他不可更改的数值（unaddressable value），只能调用value方法。
+4、对于立即数或者其他不可更改的数值（unaddressable value），只能调用value方法。  
+
+bit.OnesCount原理  
+1、先按两位一组，统计1的个数，原地存在两位中（所以可能数值为 00 01 10， 11不可能因为最多两个数）  
+2、然后将两位一组的数据加起来，变成四位一组。  
+3、重复第二步，直到只剩一组数据，就是答案。  
+
